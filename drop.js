c2c606f4faf1ad9b4f7bb816b2c6f98406bf409e
PAVIN111 <69671985+PAVIN111@users.noreply.github.com>
2021-05-04 19:49:57 +0530

    Add files via upload

---
+++ drop.js
@@ -0,0 +1,24 @@
+class Drop {
+    constructor(x, y) {
+        var options = {
+            restitution: 0.1,
+            friction: 0.01
+        }
+        this.x = x;
+        this.y = y;
+        this.radius=5
+        this.rain = Bodies.circle(x, y, 5, options)
+        World.add(world, this.rain)
+    }
+
+    display() {
+        fill("blue")
+        ellipseMode(CENTER);
+        ellipse(this.rain.position.x,this.rain.position.y,this.radius,this.radius)
+    }
+    updateY() {
+        if (this.rain.position.y > height) {
+            Matter.Body.setPosition(this.rain, { x: random(0, 400), y: random(0, 400) })
+        }
+    }
+}
